cd6bb152aa9c75d01b73ab078bdc2d88bf60572d
krasimir.triov <krasimir.triov@mentormate.com>
2016-04-15 16:27:54 +0300

    create AutomaticMigrationsToCodeBased console app and AutomaticMigrationsToCodeBased database project

---
+++ AutomaticMigrationsToCodeBased.Database/MyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity;
+
+namespace AutomaticMigrationsToCodeBased.Database
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+            : base("name=MyDbContext")
+        {
+        }
+
+        public virtual IDbSet<User> Users { get; set; }
+    }
+
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        //public string RandomProperty { get; set; }
+        //public string RandomProperty2 { get; set; }
+        //public string RandomProperty3 { get; set; }
+        //public string RandomProperty4 { get; set; }
+        //public string RandomProperty5 { get; set; }
+        //public string RandomProperty6 { get; set; }
+    }
+}
