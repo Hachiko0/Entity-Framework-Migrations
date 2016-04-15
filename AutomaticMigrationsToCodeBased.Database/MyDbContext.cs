fd665d4610d4131cb3515302914efa7621f59287
krasimir.triov <krasimir.triov@mentormate.com>
2016-04-15 16:36:19 +0300

    test combining migrations - second change

--- AutomaticMigrationsToCodeBased.Database/MyDbContext.cs
+++ AutomaticMigrationsToCodeBased.Database/MyDbContext.cs
@@ -19,9 +19,9 @@ namespace AutomaticMigrationsToCodeBased.Database
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
-        //public string RandomProperty { get; set; }
-        //public string RandomProperty2 { get; set; }
-        //public string RandomProperty3 { get; set; }
+        public string RandomProperty { get; set; }
+        public string RandomProperty2 { get; set; }
+        public string RandomProperty3 { get; set; }
         //public string RandomProperty4 { get; set; }
         //public string RandomProperty5 { get; set; }
         //public string RandomProperty6 { get; set; }
