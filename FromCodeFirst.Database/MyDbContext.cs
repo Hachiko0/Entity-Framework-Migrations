e7eaf38bf553b2e784b28fcfd51122e506e43f8b
krasimir.triov <krasimir.triov@mentormate.com>
2016-05-17 23:28:17 +0300

    fix after merge

--- FromCodeFirst.Database/MyDbContext.cs
+++ FromCodeFirst.Database/MyDbContext.cs
@@ -27,16 +27,6 @@ namespace FromCodeFirst.Database
         public string RandomProperty3 { get; set; }
         public string RandomProperty4 { get; set; }
         public string RandomProperty5 { get; set; }      
-        public int FirstDevChange { get; set; }
-        public int SecondDevChange { get; set; }
-        public int FirstDevChange1 { get; set; }
-        public int SecondDevChange1 { get; set; }
-        public int FirstDevChange2 { get; set; }
-        public int SecondDevChange2 { get; set; }
-        public int FirstDevChange3 { get; set; }
-        public int SecondDevChange3 { get; set; }
-        public int FirstDevChange4 { get; set; }
-        public int SecondDevChange4 { get; set; }
         public virtual ICollection<Role> Roles { get; set; }
     }
 
