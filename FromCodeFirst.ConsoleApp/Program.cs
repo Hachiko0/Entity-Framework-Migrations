4eedc3e8a027bac4b1d9c7e3d303038fdda59628
krasimir.triov <krasimir.triov@mentormate.com>
2016-05-18 00:07:18 +0300

    random commit so we can have more commits

--- FromCodeFirst.ConsoleApp/Program.cs
+++ FromCodeFirst.ConsoleApp/Program.cs
@@ -9,9 +9,14 @@ namespace FromCodeFirst.ConsoleApp
         {
             using (var db = new MyDbContext())
             {
+                System.Console.WriteLine(new string('=', 20));
                 var users = db.Users.ToList();
                 db.Users.Add(new User { Age = 33, FirstName = "F", LastName = "L" });
                 db.SaveChanges();
+                
+                users.ForEach(u => System.Console.WriteLine($"{u.FirstName} {u.LastName}"));
+
+                System.Console.WriteLine(new string('=', 20));
             }
         }
     }
